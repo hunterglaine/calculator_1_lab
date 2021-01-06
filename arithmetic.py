62765c0de073c7e06d4d1885423762fc9ce184dd
Hunter Laine <hunterglaine@gmail.com>
2021-01-06 23:52:19 +0000

    Complete power function

--- arithmetic.py
+++ arithmetic.py
@@ -46,6 +46,9 @@ def cube(num1):
 def power(num1, num2):
     """Raise num1 to the power of num2 and return the value."""
 
+    # Find num1 raised to the power of num2
+    # Return the result
+    return(num1 ** num2)
 
 def mod(num1, num2):
     """Return the remainder of num1 / num2."""
