c823417f2f38d20648c956a761476cba4d37a033
Hunter Laine <hunterglaine@gmail.com>
2021-01-06 23:42:07 +0000

    Complete multiply function

--- arithmetic.py
+++ arithmetic.py
@@ -19,7 +19,9 @@ def subtract(num1, num2):
 
 def multiply(num1, num2):
     """Multiply the two inputs together."""
-
+    # Multiply num1 and num2
+    # Return the result
+    return(num1 * num2)
 
 def divide(num1, num2):
     """Divide the first input by the second and return the result."""
